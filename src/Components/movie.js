c3d5484d6bd6f267b5ab2616faeb9bfb83f9cb47
Lucimara Lemes <110794157+lucimaralemes5@users.noreply.github.com>
2023-11-12 01:05:25 -0400

    Initial commit

---
+++ src/Components/movie.js
@@ -0,0 +1,37 @@
+import "./styles.scss";
+
+export default function Movie() {
+	return (
+		<>
+			<header>
+				<img src="https://images.vexels.com/media/users/3/191703/isolated/preview/3477bc55ba545dc9005d5b89f593a5ea-producao-de-filme-legal.png" alt="" />
+				<nav>
+					<ul>
+						<li>Inicio</li>
+						<li>Filmes</li>
+						<li>Séries</li>
+					</ul>
+				</nav>
+			</header>
+
+			<main>
+				<h1>Filmes</h1>
+				<section>
+					<figure>
+						<img src="https://img.elo7.com.br/product/original/23646C7/big-poster-filme-capita-marvel-tamanho-90x60-cm-presente-geek.jpg" alt="" />
+						<img src="https://img.elo7.com.br/product/original/2747CBC/big-poster-filme-aladdin-2019-lo01-tamanho-90x60-cm-nerd.jpg" alt="" />
+						<img src="https://img.elo7.com.br/product/original/259A7AA/big-poster-filme-vingadores-ultimato-lo001-tamanho-90x60-cm-poster-marvel.jpg" alt="" />
+					</figure>
+				</section>
+
+				<section>
+					<figure>
+						<img src="https://img.elo7.com.br/product/original/2677057/big-poster-filme-guardioes-da-galaxia-lo01-tamanho-90x60-cm-geek.jpg" alt="" />
+						<img src="https://img.elo7.com.br/product/zoom/2368C5D/big-poster-filme-marvel-venom-tamanho-90x60-cm-loot-op-010-geek.jpg" alt="" />
+						<img src="https://img.elo7.com.br/product/zoom/26BBFD5/big-poster-filme-marvel-fenix-negra-lo004-tamanho-90x60-cm-geek.jpg" alt="" />
+					</figure>
+				</section>
+			</main>
+		</>
+	);
+}
